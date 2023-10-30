8aa15e2403b60cabe76b7caf10d4808c5f4445cd
Bryant Moreira dos Anjos <bryant_mx@hotmail.com>
2023-10-30 01:24:46 -0300

    add rate limit for the validate otp request route

--- src/routes/TwoFactorAuthenticationRoutes.ts
+++ src/routes/TwoFactorAuthenticationRoutes.ts
@@ -1,4 +1,6 @@
 import { Elysia, t } from 'elysia'
+import { rateLimit } from 'elysia-rate-limit'
+
 import TwoFactorAuthenticationController from '../controlles/TwoFactorAuthenticationController'
 import CreateSecretKeyUseCase from '../useCases/CreateSecretKeyUseCase'
 import GetOTPUseCase from '../useCases/GetOTPUseCase'
@@ -13,11 +15,15 @@ const _2FAController = new TwoFactorAuthenticationController(
   validateOTPUseCase,
 )
 
+const ONE_MINUTE = 60000
+const REQUESTS_ALLOWED = 5
+
 export default class TwoFactorAuthenticationRoutes {
   create(app: Elysia) {
     return app
       .post('/secret', _2FAController.createSecretKey)
       .get('/otp/:secret', _2FAController.getOneTimePassword)
+      .use(rateLimit({ duration: ONE_MINUTE, max: REQUESTS_ALLOWED }))
       .post('/otp/:secret', _2FAController.validateOneTimePassword, {
         body: t.Object({
           otp: t.String(),
